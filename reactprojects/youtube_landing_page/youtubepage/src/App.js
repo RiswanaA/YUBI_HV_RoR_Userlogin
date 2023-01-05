0379608da733b9d059b8e0c63053a73695825248
RiswanaA <riswana.begam.87@gmail.com>
2023-01-05 22:27:06 +0530

    t after adding youtube landing page :q 			:wq 			:q
    												#
    
    												:q
    												:q

---
+++ reactprojects/youtube_landing_page/youtubepage/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Maindiv from './components/Maindiv';
+
+
+function App() {
+  return (
+
+    <>
+      <Header />
+      <Sidebar />
+      <Maindiv />
+
+    </>
+  );
+}
+
+export default App;
